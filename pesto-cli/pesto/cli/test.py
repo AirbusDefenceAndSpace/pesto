7dce074f40fe426d4884a3175db604f46f944522
steraite <72130836+steraite@users.noreply.github.com>
2021-03-17 17:37:18 +0100

    Update pesto-cli/pesto/cli/test.py

--- pesto-cli/pesto/cli/test.py
+++ pesto-cli/pesto/cli/test.py
@@ -5,7 +5,7 @@ from pesto.cli.core.utils import PESTO_LOG
 from pesto.common.testing.test_runner import TestRunner
 
 
-def test(build_config_path, profiles, nvidia=False, network="host"):
+def test(build_config_path, profiles, nvidia=False, network=None):
     build_config = BuildConfig.from_path(path=build_config_path, profiles=profiles, network=network)
     PESTO_LOG.info('build configuration : {}'.format(build_config))
 
